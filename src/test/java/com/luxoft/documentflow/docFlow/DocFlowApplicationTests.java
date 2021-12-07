fc529b3e6035255956c6b110793be899c05115da
Konstantin <konstantinleonov89@gmail.com>
2021-12-07 14:01:21 +0300

    Initial commit

---
+++ src/test/java/com/luxoft/documentflow/docFlow/DocFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxoft.documentflow.docFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
